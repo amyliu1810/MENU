1809480d95da58a20cad0f5c083b3bbc14b56aaa
amyliu1810 <143366312+amyliu1810@users.noreply.github.com>
2024-01-08 20:17:32 +0800

    Initial commit

---
+++ js/menu.js
@@ -0,0 +1,107 @@
+const menu = [
+    {
+        name: '恰吉 可頌＆咖啡',
+        img_path: 'https://lh3.googleusercontent.com/XvdvN8d0XqbaFtpJMRVSduy51e9lksKqIpjUwlPT4tjAtBy8ewK61-s_jtw7FsIwkHYPM01yfsCPTK7CpVix15G89KoTPi7o_aJjFjoegzaG=s360',
+        address: '臺中市南區忠孝路8號',
+        star: 4.7,
+        google_map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3641.2841148727434!2d120.67280547534766!3d24.12665847841558!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34691b2f4cf21ae5%3A0x7ebf5a173b691e49!2z5oGw5ZCJIOWPr-mgjO-8huWSluWVoSBDaHVja3kgY3JvaXNzYW50ICYgY29mZmVl!5e0!3m2!1szh-TW!2stw!4v1692772552389!5m2!1szh-TW!2stw" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+    },
+    {
+        name: '熊派 Brown Bear Pie Bakery',
+        img_path: 'https://lh3.googleusercontent.com/Kh2CGIgxBw5njwell4oeoj8RRrxcEVk2KWRFwsH4LVt9mHsFVUjBfQrREwpGds9XDo6ZqRI-ekOXMC1il4IglL79muDVCrZUEIiLaE0N_Bw=s360',
+        address: '臺中市南區濟世街60號',
+        star: 4.8,
+        google_map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3641.1527480657323!2d120.67548557534789!3d24.131273278412372!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34693d7cd1f389d1%3A0xab2e513c2e70b1b0!2z54aK5rS-IEJyb3duIEJlYXIgUGllIEJha2VyeQ!5e0!3m2!1szh-TW!2stw!4v1692773359427!5m2!1szh-TW!2stw" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+    },
+    {
+        name: '北海道小樽-禾荳家カレー屋',
+        img_path: 'https://lh3.googleusercontent.com/e50aAM5r9IbIyZd4xWLcEveMAvyL_uvfHU-gaL_7CHkGruHcwaN0EnuEhvJITH1DzBDBB62Ak41cfdhdCYY9FvYFGLQJ0OsezUdy1w0R6x4_=s360',
+        address: '臺中市大里區德芳路二段105號1樓',
+        star: 4.6,
+        google_map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14568.83360817053!2d120.67661341531789!3d24.094165593283847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34693d6defbc5599%3A0x80764be5ae5a2790!2z56a-6I2z5a62LUN1cnJ5KOWMl-a1t-mBk-a5r-WSluWTqeWwiOizo-W6lynlj7DkuK3nvo7po58g5Y-w5Lit576O6aOf5o6o6JamIOWPsOS4reWSluWTqSDlpKfph4znvo7po58g5aSn6YeM5ZKW5ZOpIOWkp-mHjOaXpeW8j-aWmeeQhiDlpKfph4zlv4XlkIMg5b636Iqz6Lev576O6aOf!5e0!3m2!1szh-TW!2stw!4v1692773403314!5m2!1szh-TW!2stw" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+    },
+    {
+        name: '鮨壽司',
+        img_path: 'https://lh3.googleusercontent.com/0Xf-zFxwgTwTNUFf7faRhco6tZMYwU9Rw3plJAZqjKIkePBypyyiv3DmVU-7cXWz7aA6bFBRuNQTdRWColYwmILxpm9Kk2MQTn628ESWTOD_=s360',
+        address: '臺中市南區公理街52號',
+        star: 4.5,
+        google_map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14564.816403309645!2d120.6559609871582!3d24.129469400000016!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34693d9de505b493%3A0x4c34b7f36bb1f9eb!2z6a6o5aO95Y-4!5e0!3m2!1szh-TW!2stw!4v1692773427970!5m2!1szh-TW!2stw" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+    },
+    {
+        name: '築崎鍋物',
+        img_path: 'https://lh3.googleusercontent.com/EGT2F5VG2Ag-pS60Ed6eY8aiJlFFX0nhC-OcJALL3NXX3vUQ6PqdUK2df18Y3j3p9ZyoHQv4GznfulGkXfHhaXm0F7_uys2hH_TYbEo2Ei1g=s360',
+        address: '臺中市南區興大路145號',
+        star: 4.6,
+        google_map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29125.71216475183!2d120.65808892646356!3d24.146679378410457!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34693d8c5a5dba15%3A0x518281fd732a78d1!2z56-J5bSO55u45pKy6Y2L54mpLeiIiOWkp-auvyAkMTcw6Ieq5Yqp6JSs6aOf5ZCD5Yiw6aO9!5e0!3m2!1szh-TW!2stw!4v1692773452114!5m2!1szh-TW!2stw" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+    },
+    {
+        name: '桃姐廚房',
+        img_path: 'https://lh3.googleusercontent.com/UjAgI7Dt4RIUtlYozla-RSf9lJ--6UfVuUu4P4wEg-sYiMSB9fTRAxR5TDRyjQviGBcnk9IsscJux-DveiWi9IyudAm_u4T2=s360',
+        address: '臺中市大里區大明路76號',
+        star: 3.9,
+        google_map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3641.604172105986!2d120.68544067609331!3d24.11541167466413!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34693ce5f01b1a85%3A0xccf7670e551090f6!2z5qGD5aeQ5bua5oi_!5e0!3m2!1szh-TW!2stw!4v1692773473370!5m2!1szh-TW!2stw"  style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+    },
+    {
+        name: '靜宜早點',
+        img_path: 'https://lh3.googleusercontent.com/xlh3V1Ds94YENvFPZWNkee-M7kFOGk36Z2SQhXZKENsNfzimQNH4tqF7eDv3RCYHUrrhlKKhQD9dlQH2AepVCg10WLpdyME=s360',
+        address: '臺中市南區復興路三段164號',
+        star: 4.2,
+        google_map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3641.1735615591338!2d120.67168797609361!3d24.13054217407482!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34693d05a8b2d033%3A0x91b6e03008d53a6d!2z6Z2c5a6c5pep6bue!5e0!3m2!1szh-TW!2stw!4v1692773493594!5m2!1szh-TW!2stw"  style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+    },
+    {
+        name: '南興肉圓',
+        img_path: 'https://i0.wp.com/yama.tw/wp-content/uploads/20221228140115_95.jpg?w=1280&quality=99&ssl=1',
+        address: '臺中市南區福德街120號',
+        star: 4.1,
+        google_map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3641.390572167689!2d120.65913498689528!3d24.12291811873988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34693c553ce8689f%3A0x8fe4d0a3bdd04870!2z5Y2X6IiI6IKJ5ZyTKOiAgeW6lyk!5e0!3m2!1szh-TW!2stw!4v1692773515769!5m2!1szh-TW!2stw" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+    },
+    {
+        name: '小彤帽密沾醬炸鷄排',
+        img_path: 'https://lh3.googleusercontent.com/gyyHSVN6DVahD8Oa_yxFO0rQBJCW6f_jSh1oUnPkr3NiPc02W3YNT_Jmwy_Ih7zp3qgGtEDfHBfqOaeWVn4hOu_4NneGN-TulzxMo9Tyjdpc=s360',
+        address: '臺中市大里區永興路153號',
+        star: 4.8,
+        google_map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3641.746164806363!2d120.67911087609326!3d24.110420474858312!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34693d1aa15fbfd1%3A0x9150fecf2510463!2z5bCP5b2k5bi95a-G5rK-6Yak54K46beE5o6SIOWkp-mHjOe4veW6lw!5e0!3m2!1szh-TW!2stw!4v1692773549057!5m2!1szh-TW!2stw" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+    },
+    {
+        name: '幸花鷄白湯拉麵',
+        img_path: 'https://lh3.googleusercontent.com/z3cCh34v8wg3jLeHoucuapzU0aHstzM93k2ZM20C0fB0KuJrhuU2IL7omsBbFKirOm-ng4L6FUKkKii2ZTJQgRktnAFpyyrvDW7YMn8wyRE=s360',
+        address: '臺中市南區國光路156之6號',
+        star: 4,
+        google_map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3641.2508700383146!2d120.67354148689621!3d24.127826418540298!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34693dc66c0e5f4f%3A0xa202d3340532fac!2z5bm46Iqx6beE55m95rmv5ouJ6bq1ICjljZfljYDlnIvlhYnlupcp!5e0!3m2!1szh-TW!2stw!4v1692773576802!5m2!1szh-TW!2stw" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+    },
+    {
+        name: '山禾堂拉麵',
+        img_path: 'https://scontent-tpe1-1.xx.fbcdn.net/v/t39.30808-6/241438457_4411267375629881_533951199515516396_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=mtGPGDzaZuQAX9lL7jK&_nc_ht=scontent-tpe1-1.xx&oh=00_AfAoxg5Qbr8gt1Q1R1qzrCmJiJqiEGGv_QxtK_bIjXrkEA&oe=65A044FD',
+        address: '412台中市大里區德芳路二段155-2號',
+        star: 4.7,
+        google_map: '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5150.366700950529!2d120.67847548770537!3d24.106445771109136!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34693d9b55c924ed%3A0xfe8e02c339befc2a!2z5bGx56a-5aCC5ouJ6bq1LeWPsOS4reW6lw!5e0!3m2!1szh-TW!2stw!4v1704714360960!5m2!1szh-TW!2stw" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+    },
+    {
+        name: '牛丁次郎坊',
+        img_path: 'https://scontent-tpe1-1.xx.fbcdn.net/v/t1.6435-9/128098002_135791028326062_1447274867952451529_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=7f8c78&_nc_ohc=JyxJW0-3r3IAX-go5BK&_nc_ht=scontent-tpe1-1.xx&oh=00_AfD_w2QNFUe_U08rdw5nzg4NP2zIf1DmEA7gChG3e0rFGQ&oe=65C345EC',
+        address: '412台中市大里區東榮路233號',
+        star: 4.8,
+        google_map: '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2575.1218248551927!2d120.68358204633584!3d24.109504870689374!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34693d50b54f89af%3A0xa9be6f7c6667f11a!2z54mb5LiB5qyh6YOO5Z2KeOWkp-mHjOW6lw!5e0!3m2!1szh-TW!2stw!4v1704714642477!5m2!1szh-TW!2stw" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+    },
+];
+
+const box = document.querySelector('.box');
+
+menu.forEach((item) => {
+    console.log(item.img_path);
+    box.innerHTML += `
+    <div class="card">
+        <div class="card-body">
+            <div class="img_path">
+                <img src="${item.img_path}"
+                    alt="恰吉 可頌＆咖啡">
+            </div>
+            <div class="title">${item.name}</div>
+            <div class="address">${item.address}</div>
+            <div class="star">${item.star} 星
+                <span class="yellow"></span>
+            </div>
+            <div class="map">${item.google_map}</div>
+        </div>
+    </div>`;
+});
